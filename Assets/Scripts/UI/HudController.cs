bb006e15a6cab44512ed1c5dec5e67af8e3ebd9c
Joey Meyer <joeymeyer1986@gmail.com>
2016-12-03 22:47:49 -0600

    player 1 health bar setup

---
+++ Assets/Scripts/UI/HudController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour {
+	public Fighter player1;
+	public Fighter player2;
+
+	public Text leftText;
+	public Text rightText;
+
+	// Use this for initialization
+	void Start () {
+		leftText.text = player1.fighterName;
+		rightText.text = player2.fighterName;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
